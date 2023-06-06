f4c3209b95138fb0aeccbcea635a14c8c04b2496
Paul Arthur <paul.arthur@flowerysong.com>
2023-06-06 01:28:47 -0400

    Add missing redirect

--- renenutet/urls.py
+++ renenutet/urls.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/ui')),
     path('api/', include('api.urls')),
     path('ui/', include('ui.urls')),
     path('admin/', admin.site.urls),
